63c7d40aa5b8a1f8b777f7b11998e1b381a2699a
polinchesta <polinalashkevich72@gmail.com>
2022-11-29 21:43:37 +0300

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 import board from "./components/board.js";
 import complain from "./components/complain";
 import additionally from "./components/additionally";
-import cards from "./components/cards/Card";
-import index from "./components/cards/index";
+import cards from "./components/cards";
 
 document.querySelector(".boards-wrapper")?.append(board.element);
 cards.getCards();
